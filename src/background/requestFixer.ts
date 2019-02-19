9430abc4d659350561cfca430ce1ee77d98c41b1
Gil Amran <gil@orbs.com>
2019-02-19 13:33:55 +0200

    fixed requestFixer for chrome 72

--- src/background/requestFixer.ts
+++ src/background/requestFixer.ts
@@ -1,21 +1,33 @@
 function updateRequest(headers, name: string, value: string) {
-	let found = false;
-	for (var n in headers) {
-		found = headers[n].name.toLowerCase() === name.toLowerCase();
-		if (found) {
-			headers[n].value = value;
-			return;
-		}
-	}
-	if (!found) {
-		headers.push({ name, value });
-	}
+  let found = false;
+  for (var n in headers) {
+    found = headers[n].name.toLowerCase() === name.toLowerCase();
+    if (found) {
+      headers[n].value = value;
+      return;
+    }
+  }
+  if (!found) {
+    headers.push({ name, value });
+  }
 }
 
 export function fixRequest() {
-    chrome.webRequest.onBeforeSendHeaders.addListener(details => {
-        updateRequest(details.requestHeaders, "Referrer", "https://app.meckano.co.il/login.php");
-        updateRequest(details.requestHeaders, "Origin", "https://app.meckano.co.il");
-        return { requestHeaders: details.requestHeaders };
-    }, { urls: ["https://app.meckano.co.il/api/*"] }, ["requestHeaders", "blocking"]);
+  chrome.webRequest.onBeforeSendHeaders.addListener(
+    details => {
+      updateRequest(
+        details.requestHeaders,
+        'Referrer',
+        'https://app.meckano.co.il/login.php'
+      );
+      updateRequest(
+        details.requestHeaders,
+        'Origin',
+        'https://app.meckano.co.il'
+      );
+      return { requestHeaders: details.requestHeaders };
+    },
+    { urls: ['https://app.meckano.co.il/api/*'] },
+    ['requestHeaders', 'blocking', 'extraHeaders']
+  );
 }
